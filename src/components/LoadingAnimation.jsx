2494b1113e57f10ace4a27a844315cd06953cf86
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-17 19:24:51 +0100

    Update LoadingAnimation.jsx

--- src/components/LoadingAnimation.jsx
+++ src/components/LoadingAnimation.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class LoadingAnimation extends Component {
 	render() {
 		return <div className="flex justify-center items-center bg-[#003045] h-screen">
-    <div className="w-16 h-16 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
+    <div className="w-16 h-16 border-t-4 border-purple-600 border-solid rounded-full animate-spin"></div>
   </div>;
 	}
 }
