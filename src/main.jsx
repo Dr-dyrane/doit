85feef3fc00bd830e1b900ca5c99d358b100bfa3
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-09-24 16:24:54 +0100

    remove service worker

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,6 @@ import "./assets/index.css";
 
 const rootElement = document.getElementById("root");
 
-async function registerServiceWorker() {
-  if ("serviceWorker" in navigator) {
-    try {
-      const registration = await navigator.serviceWorker.register("/service-worker.js");
-      console.log("Service Worker registered with scope:", registration.scope);
-    } catch (error) {
-      console.error("Service Worker registration failed:", error);
-    }
-  }
-}
-
-// Register the service worker when the page loads
-window.addEventListener("load", registerServiceWorker);
-
 // Create a root using ReactDOM.createRoot
 if (!rootElement._reactRootContainer) {
   const root = ReactDOM.createRoot(rootElement);
